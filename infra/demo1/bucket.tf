9e1d4ea5a90944ae00b3987640f725987d4a37ec
murali <murali>
2023-03-21 16:56:52 +0000

    initial commit

--- infra/demo1/bucket.tf
+++ infra/demo1/bucket.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "auto-expire" {
   name          = "ms-one-no-public-access-bucket"
+  project = "nited-lane-354904"
   location      = "US"
   force_destroy = true
 
