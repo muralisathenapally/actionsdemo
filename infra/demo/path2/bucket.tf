21148336374ee1123ccf9fcc5ca7f8e85a50d85c
muralisathenapally <78802406+muralisathenapally@users.noreply.github.com>
2023-06-21 22:21:21 +0530

    Update bucket.tf

--- infra/demo/path2/bucket.tf
+++ infra/demo/path2/bucket.tf
@@ -1,7 +1,7 @@
 module "gcs_buckets" {
   source     = "git::https://github.com/muralisathenapally/tfm.git?ref=artifact_registry"
   project_id  = "united-lane-354904"
-  names = ["msfirstbktgu", "mssecondbktfyu"]
+  names = ["msfirstbktg", "mssecondbktfyu"]
   versioning = {
     first = true
   }
