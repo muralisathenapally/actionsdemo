1e757d552fd4eae1f4fb1a748603f7560d6c74d8
muralisathenapally <78802406+muralisathenapally@users.noreply.github.com>
2025-03-20 11:33:51 +0530

    asdg

--- infra/demo/path1/test/new/bucket.tf
+++ infra/demo/path1/test/new/bucket.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "auto-expire" {
-  name          = "msaaa-two-no-public-access-bucket"
+  name          = "msaaab-two-no-public-access-bucket"
   project = "psyched-circuit-405312"
   location      = "US"
   force_destroy = true
