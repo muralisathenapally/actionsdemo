f92f904a260cefe840f9eaa2dbfe373fbab3800e
muralisathenapally <78802406+muralisathenapally@users.noreply.github.com>
2025-03-20 11:12:35 +0530

    changes

--- infra/demo1/bucket.tf
+++ infra/demo1/bucket.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "auto-expire" {
-  name          = "msrth-one-no-public-access-bucket"
+  name          = "msrth-ne-no-public-access-bucket"
   project = "psyched-circuit-405312"
   location      = "US"
   force_destroy = true
