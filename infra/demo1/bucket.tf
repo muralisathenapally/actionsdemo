7944f4baa667832634b6adddab7c3ed0563dd56d
muralisathenapally <78802406+muralisathenapally@users.noreply.github.com>
2025-09-06 19:14:55 +0530

    Update bucket.tf

--- infra/demo1/bucket.tf
+++ infra/demo1/bucket.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "auto-expire" {
-  name          = "msrtlh-ne-no-public-access-bucket"
+  name          = "mslh-ne-no-public-access-bucket"
   project = "psyched-circuit-405312"
   location      = "US"
   force_destroy = true
