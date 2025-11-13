356807c7d5d5aaf7a3c08501d5b721fe82fb1401
muralisathenapally <78802406+muralisathenapally@users.noreply.github.com>
2025-11-13 09:05:57 +0530

    changes

--- infra/dev/path1/bucket.tf
+++ infra/dev/path1/bucket.tf
@@ -1,9 +1,8 @@
 resource "google_storage_bucket" "auto-expire" {
-  name          = "msrtlh-ne-no-public-access-bucket"
+  name          = "s-thr-no-public-access-bucketsdfgsdfg"
   project = "psyched-circuit-405312"
   location      = "US"
   force_destroy = true
 
   public_access_prevention = "enforced"
 }
-####
--- infra/prod/bucket.tf
+++ infra/prod/bucket.tf
@@ -1,8 +1,9 @@
 resource "google_storage_bucket" "auto-expire" {
-  name          = "s-thr-no-public-access-bucket"
+  name          = "msrtlh-ne-no-publisdfgsdfgc-access-bucket"
   project = "psyched-circuit-405312"
   location      = "US"
   force_destroy = true
 
   public_access_prevention = "enforced"
 }
+####
